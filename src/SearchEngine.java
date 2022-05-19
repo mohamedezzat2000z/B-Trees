00dc47b8ece1c88f8efc81cdac01b7940bdaddc1
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2022-05-19 18:14:45 +0200

    search engine start

---
+++ src/SearchEngine.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class SearchEngine implements ISearchEngine {
+
+    @Override
+    public void indexWebPage(String filePath) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void indexDirectory(String directoryPath) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteWebPage(String filePath) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<ISearchResult> searchByWordWithRanking(String word) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<ISearchResult> searchByMultipleWordWithRanking(String sentence) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
