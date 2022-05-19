3e7feffc320abd148ba71a2ac37926a32d358311
aaserfawzy <aaserkind537@gmail.com>
2022-05-19 10:42:05 +0200

    init

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,65 +1,88 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BTreeNode<K extends Comparable<K>, V> implements IBTreeNode<K,V> {
 
+    List<K> keys= new ArrayList<K>();
+    List<V> value= new ArrayList<V>();
+    BTreeNode<K,V> parent;
+    List<IBTreeNode<K,V>> childs= new ArrayList<IBTreeNode<K,V>>();
+    int numberOfKeys;
+    Boolean last; /*to keep track wherther to put the new array at start or last */
+
     @Override
     public int getNumOfKeys() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.keys.size();
     }
 
     @Override
     public void setNumOfKeys(int numOfKeys) {
-        // TODO Auto-generated method stub
-        
+      this.numberOfKeys=numOfKeys;
     }
 
     @Override
     public boolean isLeaf() {
-        // TODO Auto-generated method stub
+        if(childs.size()>0){
+            return true;
+        }
         return false;
     }
 
     @Override
     public void setLeaf(boolean isLeaf) {
-        // TODO Auto-generated method stub
-        
+         // TODO Auto-generated method stub
     }
 
     @Override
     public List<K> getKeys() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.keys;
     }
 
     @Override
     public void setKeys(List<K> keys) {
-        // TODO Auto-generated method stub
+        if( (this.keys.size() > 0)  &&  ( this.keys.get(0).compareTo(keys.get( keys.size()-1))) > 0 ){
+            this.last=false;
+            List<K> temp = keys;
+            temp.addAll(this.keys);
+            this.keys=temp;
+        }else{
+            this.keys.addAll(keys);
+            this.last=true;
+        }
         
     }
 
     @Override
     public List<V> getValues() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.value;
     }
 
     @Override
     public void setValues(List<V> values) {
-        // TODO Auto-generated method stub
+      if(this.last=true){
+            this.value.addAll(values);
+      }else{
+        List<V> temp = values;
+        temp.addAll(this.value);
+        this.value=temp;
+      }
         
     }
 
     @Override
     public List<IBTreeNode<K, V>> getChildren() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.childs;
     }
 
     @Override
     public void setChildren(List<IBTreeNode<K, V>> children) {
-        // TODO Auto-generated method stub
-        
+        if(this.last=true){
+            this.childs.addAll(children);
+      }else{
+        List<IBTreeNode<K, V>> temp = children;
+        temp.addAll(this.childs);
+        this.childs=temp;
+      }
     }
     
 }
