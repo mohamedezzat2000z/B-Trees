0afba9a5ba63361cc9629d2feef1060cff806866
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2022-05-21 23:10:34 +0200

    bencnesh

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BtreeBenchmark.benchTree(100000000, "bench 100000000");
-        // SearchEngineBenchmark.benchSearch("he", "he won the cup", "bench he");
+        // BtreeBenchmark.benchTree(100000000, "bench 100000000");
+        SearchEngineBenchmark.benchSearch("he",
+                "I am not ok please send help as fast as possible my mind slowly losing",
+                "bench 100 words");
 
     }
 }
